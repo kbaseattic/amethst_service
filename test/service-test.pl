f7cce5f4e8d73b58ee91c796a4024ccf8d921317
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2014-04-14 13:28:49 -0500

    added service-test

---
+++ test/service-test.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Bio::KBase::AmethstService::AmethstServiceImpl;
+
+
+
+
+my $amethst_obj = new Bio::KBase::AmethstService::AmethstServiceImpl('shocktoken' => 'fake');
+
+
+my $amethst_service_version = $amethst_obj->version();
+
+
+if (defined $amethst_service_version && $amethst_service_version ne '') {
+	print "amethst service return version ".$amethst_service_version."\n";
+	exit(0);
+}
+
+print STDERR "error: amethst service did not return version information\n";
+
+exit(1);
